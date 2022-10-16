7d5cf90ef294eb46ec65edb9bdb8044b18789487
Reem Ahmed <reem.mda0909@gmail.com>
2022-10-16 16:40:58 -0600

    set up backend node and install dependencies

---
+++ node-api/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express()
+const db = require('./db')
+const port = 3000
+
+app.use(bodyParser.json())
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+});
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+});
+
+app.use('/api/v1/users',userRoutes);
---
+++ node-api/src/users/queries.js
@@ -0,0 +1,15 @@
+const getUsers = 'SELECT * FROM users ORDER BY id ASC';
+const createUser = 'INSERT INTO users (location, alias, email) VALUES ($1, $2, $3)';
+const getUserById = 'SELECT * FROM users WHERE id = $1';
+const getUserByEmail = 'SELECT * FROM users WHERE email = $1';
+const updateUser = 'UPDATE users SET location = $1, alias = $2, email = $3 WHERE id = $4';
+const deleteUser = 'DELETE FROM users WHERE id = $1';
+
+module.exports = {
+    getUsers, 
+    createUser,
+    getUserById,
+    updateUser,
+    deleteUser,
+    getUserByEmail
+}
