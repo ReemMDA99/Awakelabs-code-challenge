14240119b90bc0640a2c91efb0ee56b970ad0f8a
Reem Ahmed <reem.mda0909@gmail.com>
2022-10-17 12:31:19 -0600

    create user data services

--- react-basic-crud-app/src/services/user.service.js
+++ react-basic-crud-app/src/services/user.service.js
@@ -1,1 +1,24 @@
-import { _get, _post, _put, _delete } from "../http-common"
+import { _get, _post, _put, _delete } from "../http-common";
+
+class UserDataService {
+    getAll() {
+        return _get("users");
+    }
+    get(id) {
+        return _get(`users/${id}`);
+    }
+    create(data) {
+        return _post(data, 'users')
+    }
+    update(id, data) {
+        return _put(data, `user/${id}`);
+    }
+    delete(id) {
+        return _delete(`users/${id}`);
+    }
+    findByEmail(email) {
+        return _get(`users?email=${email}`);
+    }
+}
+
+export default new UserDataService();
